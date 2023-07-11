4c3ee533a7cf31c416dae9f008270833e89ba9bc
JeaNugroho <jeanugroho@yahoo.com>
2023-07-11 00:02:55 -0500

    Develop screen for bilingual reading

---
+++ src/components/BilingualItem.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="columns-2 py-1">
+        <div class="px-10">
+            <p class="text-lg font-semibold">{{ verse.english }}</p>
+        </div>
+        <div class="px-10">
+            <p class="text-lg">{{ verse.indo }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['verse']
+}
+</script>
+
+<style>
+</style>
---
+++ src/pages/ReadingPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="">
+        <bilingual-item v-for="verse in verses" :key="verse" :verse="verse"></bilingual-item>
+    </div>
+</template>
+
+<script>
+import BilingualItem from '../components/BilingualItem.vue'
+
+export default {
+    components: {
+        BilingualItem
+    },
+    props: ['verses']
+}
+</script>
+
+<style></style>
