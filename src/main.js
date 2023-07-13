b2c768320eef2ff5ab707d4ba5d38ae73f28831b
JeaNugroho <jeanugroho@yahoo.com>
2023-07-12 19:50:27 -0500

    Install Fontawesome

--- src/main.js
+++ src/main.js
@@ -2,6 +2,22 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 
-const app = createApp(App)
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
 
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faUserSecret)
+
+/* use like this in any component
+<font-awesome-icon icon="fa-solid fa-user-secret" />
+*/
+
+const app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
