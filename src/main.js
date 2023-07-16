15e4519cbd481a49735e803e45a067ce7a5c16e4
JeaNugroho <jeanugroho@yahoo.com>
2023-07-15 21:07:45 -0500

    Make basic main view

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config'
+// import { Icon } from 'primevue/icons'
+import 'primevue/resources/themes/viva-light/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
 import App from './App.vue'
 import './index.css'
+
+import Button from 'primevue/button';
 
 /* import the fontawesome core */
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -17,7 +25,9 @@ library.add(faUserSecret)
 /* use like this in any component
 <font-awesome-icon icon="fa-solid fa-user-secret" />
 */
-
 const app = createApp(App)
+app.use(PrimeVue)
+app.component('prime-vue-button', Button)
+// app.component('prime-vue-icon', Icon)
 app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
